aef824cca39a9cb87053c3dd745301071b213ae7
arthur-lopes <59693644+arthur-lopes@users.noreply.github.com>
2020-02-28 09:04:11 -0300

    Criado index.js e adicionado readline-sync

---
+++ index.js
@@ -0,0 +1,14 @@
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+
+
+    // Pergunta pelo termo de busca e retorna a busca
+    function askAndReturnSearchTerm() {
+        return 'TERM EXAMPLE'
+    }
+    console.log(content)
+}
+
+start()
