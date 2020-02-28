99f790596c27d7686d6d7713a377d1493e277472
arthur-lopes <59693644+arthur-lopes@users.noreply.github.com>
2020-02-28 10:44:39 -0300

    Função de busca por negócio concluída com sucesso

--- robots/text.js
+++ robots/text.js
@@ -1,5 +1,18 @@
+const google = require('googleapis').google
+const customSearch = google.customsearch('v1')
+
+const googleSearchCredentials = require('../credentials/google-search.json')
+
 async function robot(content) {
-    await 
+    const response = await customSearch.cse.list({
+        auth: googleSearchCredentials.apiKey,
+        cx: googleSearchCredentials.searchEngineId,
+        q: 'Restaurante Volta Redonda',
+        num: 10
+    })
+
+    console.dir(response, {depth: null})
+    process.exit(0)
 }
 
 module.exports = robot
